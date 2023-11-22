2153439e47de60f6a44dc4e13325b9e5ee60cabf
RobiiiHUN <58343667+RobiiHUN@users.noreply.github.com>
2023-11-22 10:19:56 +0100

    Initial commit

---
+++ hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+ int i = 0;
+ char z[] = "w";
+ 
+ while (i<4)
+ {
+    printf("%c", z[0] * i);
+    i ++;
+ }
+ 
+
+
+
+
+}
---
+++ printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+  printf("a = %d, b = %d, sum = a + b = %d + %d = %d \n",3, 5, 3, 5, 3+5);
+   
+    
+}
+
+
+//34
